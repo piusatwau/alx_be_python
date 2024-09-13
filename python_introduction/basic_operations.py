871e292764f0697f020180d66f386fc8b2eb7366
piusatwau <atwaupius7@gmail.com>
2024-09-13 11:41:20 +0530

    basic arithmetic in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+add_result = number1 + number2
+subtraction_result = number1 - number2
+product_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {add_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+
