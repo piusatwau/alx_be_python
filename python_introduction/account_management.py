642f1afb7627784d8cb8256e6ab9f7644a5e8f20
piusatwau <atwaupius7@gmail.com>
2024-09-13 17:36:08 +0530

    simle pyhon program for user to manage their personal account

---
+++ python_introduction/account_management.py
@@ -0,0 +1,21 @@
+# This simple python prgram helps one manage their bank account on ATM
+'''
+person walks to atm machine
+selects option button
+list of options is displayed 
+    1. check account balance
+    2. deposit money
+    3. withdraw money
+    4. print a mini bank statement
+    5. exit
+person can check any security concerns with their bank account
+
+
+PLAN
+we need account balance variable
+starts with 0
+when money is deposited it increases by deposit amout i.e deposit amount plus the previous balance
+when money is withdrawn, account balance decreases i.e initial minus the value entered for withdraw
+
+
+'''
