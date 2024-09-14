9790421a91bf1eaf7584dcb79bed307e7dfe6ff2
piusatwau <atwaupius7@gmail.com>
2024-09-14 11:59:27 +0530

    using datetime module to automate timezone determination

---
+++ python_introduction/printing_date_time.py
@@ -0,0 +1,23 @@
+#using datetime module in python to display date, day and time
+
+
+import datetime
+
+# getting current time
+
+current_time = datetime.datetime.now().time()
+print(f"The time is {current_time}")
+
+#getting current date
+
+current_date = datetime.datetime.today().date()
+print(f"Today is {current_date}")
+
+#getting current year
+
+current_year = datetime.datetime.today().year
+print(f"The year is {current_year}")
+
+# time in utc
+utc_time = datetime.datetime.now(datetime.UTC)
+print(f"The time in UTC is {utc_time}")
