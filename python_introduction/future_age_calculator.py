364e172a16cd7178968da2638150d6e3aa814e3b
piusatwau <atwaupius7@gmail.com>
2024-09-14 11:25:50 +0530

    taking user input and computing their future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+#taking user input and using it to calculate their age 
+
+age = int(input(print("How old are you? ")))
+future_age = age + 27
+
+print(f"In 2050, you will be {future_age} years old")
