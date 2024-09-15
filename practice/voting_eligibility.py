1c9e976c197dd31ed7d069100d2b22c89ff262af
piusatwau <atwaupius7@gmail.com>
2024-09-16 00:03:37 +0530

    using if else and elif in python

---
+++ practice/voting_eligibility.py
@@ -0,0 +1,9 @@
+# this simple python program checks if user is eligible to vote based on their age
+
+age = int(input("Enter your age: "))
+
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are below voting age")
