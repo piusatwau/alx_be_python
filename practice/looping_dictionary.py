bfd29329b209925b31dcf9f909066faba110c206
piusatwau <atwaupius7@gmail.com>
2024-09-16 17:56:48 +0530

    iterating through dictionary items using for loop. Interesting using dictionary methods like .values right inside the loop condition

---
+++ practice/looping_dictionary.py
@@ -0,0 +1,14 @@
+# looping through dictionary
+
+dictionary = {
+    "age": 20,
+    "first_name": "Pius",
+    "second_name": "Atwau",
+    "gender": "male",
+}
+
+for item in dictionary.values():
+    print(item)
+    
+    
+#it is interesting t dicover the item in the for item in dictionary is actually a variable that stores the values we are about to iterate through
