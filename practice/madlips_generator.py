0a337b24bb2c09613644ac880c1ca911aaa99403
piusatwau <atwaupius7@gmail.com>
2024-09-17 22:12:42 +0530

    very interesting madlips generator python program

---
+++ practice/madlips_generator.py
@@ -0,0 +1,9 @@
+noun = input("Please enter any noun that first comes to your mind: ")
+adverb = input("Now enter any adverb that comes ext to your mind: ")
+print("Good job so far, but you still have a long way to make a comprehensive story")
+full_name = input("Let's continue, now enter your first and last name: ")
+adjective = input("Now enter any adjective that comes in your min: ")
+
+print("You made it, here is the full story you have created in just a minute. I hope you enjoy the story \n")
+
+print(f"On a beautiful {adjective} day, I went to the zoo. I saw a funny {full_name} monkey swinging from the trees. Then, I spotted a majestic {noun} lion lounging in the sun.  What a wild and {adverb} experience! ")
