2e4cf8273e9f09f68b7e4544f5123039f0ce7f1b
piusatwau <atwaupius7@gmail.com>
2024-09-19 00:01:17 +0530

    experimenting with for loop

---
+++ practice/for_loop_challenge.py
@@ -0,0 +1,8 @@
+#using for loops to iterate through items, then add the next item to stored item and prin the current total
+
+numbers = [1, 2, 3, 4, 5]
+total = 0
+
+for number in numbers:
+    total += number
+    print(total)
---
+++ practice/for_loop_with_range.py
@@ -0,0 +1,2 @@
+for number in range(1, 20):
+    print(number)
