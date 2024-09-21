b85231c9daa899666c01d176efca9aaeb1a70dec
piusatwau <atwaupius7@gmail.com>
2024-09-21 23:10:37 +0530

    not yet sure the source of the error

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,22 +3,22 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
 
-operator = input("Choose the operation (+, -, *, /):")
+operator = input("Choose the operation (+, -, *, /)")
 
 match operator:
-    case"+":
+    case "+":
         result = num1+num2
         print(f"The result is {result}")
         
-    case"*":
+    case "*":
         result = num1*num2
         print(f"The result is {result}")
         
-    case"-":
+    case "-":
         result = num1-num2
         print(f"The result is {result}")
         
-    case"/":
+    case "/":
         if num2 == 0:
             print("Cannot dvide by 0")
         else:
