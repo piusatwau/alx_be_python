090e1679d75dfa5b6be844a8857786ed79f76b0d
piusatwau <atwaupius7@gmail.com>
2024-09-23 17:30:11 +0530

    list and sample methods

---
+++ week05/lists.py
@@ -0,0 +1,18 @@
+fruits = ["mangoes", "oranges", "Apples", "Banana"]
+print(fruits)
+
+# list methods
+
+# removing last item using pop
+
+fruits.pop()
+print(fruits)
+
+# removing an item in the list by its index using pop()
+
+fruits.pop(2)
+print(fruits)
+
+# using remove method to remove any item in the list by its value
+fruits.remove("mangoes")
+print(fruits)
