07ab2fac92e64a9fdc0c12c34d80f7b462994155
piusatwau <atwaupius7@gmail.com>
2024-09-25 00:08:20 +0530

    manipulating dictionary items

---
+++ week05/favorite_books.py
@@ -0,0 +1,16 @@
+favorite_books = {
+    "title": "rich dad poor dad",
+    "author": "robert kiyosaki",
+    "genre": "personal finances"
+}
+
+#displaying all the items in a given dictionary
+
+print(favorite_books.items())
+
+#returning dictionary keys
+print(favorite_books.keys())
+
+#returning dictionary values
+print(favorite_books.values())
+
