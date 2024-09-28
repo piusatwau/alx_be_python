11ad1255f10132929f8ba73cdf259459078fa5fc
piusatwau <atwaupius7@gmail.com>
2024-09-28 15:17:06 +0530

    functions to facilitate my indepth understanding of the building blocks behind python libraries, packages, modules and functions, classes and methods

---
+++ useful_python_functions.md
@@ -0,0 +1,6 @@
+# Useful python functions
+## These are for purposes of disecting the different components of Python libraries, modules etc
+
+- dir() - lists all the different functions in a module, likewise all the different modules in a package
+
+- global() - lists all the objects in python global namespace dictionary
---
+++ week05/understanding_scope.py
@@ -0,0 +1,11 @@
+# python scopes are implemented as dictionaries that map objets to their names
+
+# we can check scope by using builtin functions
+
+# checking global scope
+
+print(globals()) # lists all the objects in the global scope
+
+print("\n")
+
+print(dir(__builtins__))  # Lists all built-in functions and exceptions
