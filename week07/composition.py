49835ae01b3abb1a8e6f3d73f62644d25a86a734
piusatwau <atwaupius7@gmail.com>
2024-10-10 10:58:49 +0530

    added comments on composition

--- week07/composition.py
+++ week07/composition.py
@@ -1,3 +1,9 @@
+# In Python, composition is a concept in object-oriented programming where one class is composed of one 
+# or more objects from other classes. It is used to model a "has-a" relationship, meaning that a class 
+# can have objects of other classes as part of its attributes. This allows for the reuse of code and promotes 
+# modular design by breaking down complex systems into smaller, manageable pieces.
+
+
 class Car:
     def __init__(self, model, year, color):
         self.model = model
@@ -26,5 +32,5 @@ my_car = Car("Mercedes G-Class", 2022, "Black")
 my_tyre = Tyre(10, 200)
 Mercedes1 = Mercedes(my_car, my_tyre)
 
-print(Mercedes1)
+print(Mercedes1.__str__())
     
