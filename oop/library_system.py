2b38a7627258ecc676293c11a7edfe2ad7d5ec35
piusatwau <atwaupius7@gmail.com>
2024-10-12 22:58:27 +0530

    error fix

--- oop/library_system.py
+++ oop/library_system.py
@@ -17,9 +17,9 @@ class EBook(Book):
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
-        self.page_count = page_count
-    def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+    #     self.page_count = page_count
+    # def __str__(self):
+    #     return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
     
     
 # composition in python simply means the composing class takes the prior classes as its arguments
