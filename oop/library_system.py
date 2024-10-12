6aa9691c0006ca13d5c874e18cd18403c44aebb0
piusatwau <atwaupius7@gmail.com>
2024-10-12 22:51:27 +0530

    error fix

--- oop/library_system.py
+++ oop/library_system.py
@@ -34,7 +34,7 @@ class Library:
              if isinstance(book, EBook):
                 print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
              elif isinstance(book, PrintBook):
-                print(f"Print Book: {book.title} by {book.author}, Pages: {book.page_count}")
+                print(f"Print Book: {book.title} by {book.author}, Page Count: {book.page_count}")
              else:
                 print(f"Book: {book.title} by {book.author}")
 
