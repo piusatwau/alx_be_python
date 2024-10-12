7dafe37606cfcbb243b1442fbcef689289b7f5a5
piusatwau <atwaupius7@gmail.com>
2024-10-12 21:56:33 +0530

    error fix

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,13 +1,22 @@
 class Calculator:
-    calculator_type = "Arithmetics Operations"
+    # Class attribute
+    calculator_type = "Arithmetic Operations"
+    
+    def __repr__(self):
+        # Returning a string representation of the object
+        return f"Calculator: {self.calculator_type}"
+    
     def __init__(self, a, b):
         self.a = a
         self.b = b
+    
+    # Static method to add two numbers
     @staticmethod
     def add(a, b):
         return a + b
+    
+    # Class method to multiply two numbers and print the class attribute
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculator_type}")
         return a * b
-    
