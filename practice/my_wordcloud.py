813534c0685a4ccfefe68e35905fa47bd7663840
piusatwau <atwaupius7@gmail.com>
2024-11-11 12:15:19 +0530

    more practice

--- practice/my_wordcloud.py
+++ practice/my_wordcloud.py
@@ -1,6 +1,7 @@
 # Import necessary libraries
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import random
 
 # Sample text (you can replace this with your own text)
 text = """
@@ -9,7 +10,7 @@ machine learning, and web development. It is also very easy to learn!
 """
 
 # Create a WordCloud object
-wordcloud = WordCloud(width=400, height=200, background_color='white').generate(text)
+wordcloud = WordCloud(width=400, height=200, background_color='white').generate(random())
 
 # Display the generated word cloud using matplotlib
 plt.figure(figsize=(10, 5))
